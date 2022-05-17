2a566fe37674b70eb3709859d24fac9319226495
aircraftgraft <aircraftgraft@gmail.com>
2022-05-17 16:41:40 -0400

    the red car is ready with some basic features

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,13 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         BasicFeatures car = new BasicFeatures();
-        
+        car.Color = "red";
+        car.doors = "4";
+        car.engineCapacity = "v8";
+        car.headlights = "blue";
+        car.regNo = "45h97";
+
+
     }
 
 
